e664d608d5818f34626354dcc5c375e64d5565b1
Michael <spitsinmichael@gmail.com>
2018-09-08 10:48:37 +0300

    Initial commit

---
+++ app/src/main/java/com/github/programmerr47/flickrawesomeclient/FlowActivity.kt
@@ -0,0 +1,12 @@
+package com.github.programmerr47.flickrawesomeclient
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FlowActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flow)
+    }
+}
